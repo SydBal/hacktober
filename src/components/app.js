2ea9dbfd31ee80d88bea63ac3931d97c6ee2fc0c
SydBal <dominic.balassone@gmail.com>
2018-10-25 21:39:17 -0700

    starting webpack setup

---
+++ src/components/app.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+import '../styles/app.css';
+
+class app extends Component {
+    render() {
+        return (
+            <div>
+                <h1>My React App!</h1>
+            </div>
+        );
+    }
+}
+
+export default app;
